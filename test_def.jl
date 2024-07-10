7fb86b6f98ce2708b35797aefd04776069ad47df
celaira <lauled@gmx.de>
2024-07-10 16:34:30 +0200

    visuaize test not working !

--- test_def.jl
+++ test_def.jl
@@ -11,10 +11,10 @@ using Test                          # import julia's test module
     end
 
     @testset "Maze" begin                                        # testing MazeStruct
-        maze_nodes = Matrix{Node}(undef, 3, 3)
+        maze_nodes = Matrix{Node}(undef, 3, 4)
         maze = Maze(maze_nodes, nothing, nothing)
         
-        @test size(maze.nodes) == (3, 3)
+        @test size(maze.nodes) == (3, 4)
         @test maze.visual === nothing
         @test maze.path === nothing
     end
@@ -31,12 +31,14 @@ end
         solved_maze = solve(maze(8,7))
         @test length(solved_maze.path) > 0
         @test unique(solved_maze.path) == solved_maze.path              # all nodes in path only once
+        @test solved_maze.path[1] != solved_maze.path[end]
     end
 
     @testset "visualize" begin                                          # testing visualize
-        maze1 = maze(5, 5)
+        maze1 = maze(5, 7)
 
         # capture output
+        original_stdout=stdout
         io = IOBuffer()                     # standard output stream.
         redirect_stdout(io) do              # Any printed-output will be redirected into buffer
             visualize(solve(maze1))
@@ -46,5 +48,6 @@ end
         # output should contain ██ and ░░
         @test occursin("██", output)
         @test occursin("░░", output)
+        redirect_stdout(original_stdout)
     end
 end
