0d379e3faa1b3cb9a21b6bbdcf21fda1cef261d8
celaira <lauled@gmx.de>
2024-07-11 10:40:19 +0200

    mazeviz

--- src/visualize.jl
+++ src/visualize.jl
@@ -1,6 +1,8 @@
 include("Node.jl")
 
-MazeViz=Nothing
+mutable struct MazeViz
+    output::Union{String,Nothing}
+end
 
 mutable struct Maze
     nodes::Matrix{Node}
@@ -9,7 +11,7 @@ mutable struct Maze
 end
 
 # Task 4
-function visualize(matrix::Matrix{Node}, Path::Union{Vector{Node},Nothing})
+function visualize(matrix::Matrix{Node}, Path::Union{Vector{Node},Nothing}) # Funktion zur visualisierung
     width = size(matrix)[2]
     height = size(matrix)[1]
     wall = "██"
@@ -19,28 +21,28 @@ function visualize(matrix::Matrix{Node}, Path::Union{Vector{Node},Nothing})
     if Path===nothing
         s=[]
     else
-        s=Vector{Int}[node.key for node in Path]
+        s=Vector{Int}[node.key for node in Path] # um in späterer abfrage den Path zu erstellen
     end
 
-    n=Vector{Union{Vector{Int},Nothing}}[[node.neighbors[i].key for i=1:4] for node in matrix]
-    for node in n
+    n=Vector{Union{Vector{Int},Nothing}}[[node.neighbors[i].key for i=1:4] for node in matrix] # Alle Nodes
+    for node in n # geht alle Nodes durch
         for i=1:4
-            if node[i]==[0] || node[i]==[-1]
+            if node[i]==[0] || node[i]==[-1] # Falls Nodes keine Nachbarn haben -> setzt auf Nothing
                 node[i]=nothing  
             end
         end
     end
     
-    for i=1:2*width+1
+    for i=1:2*width+1 # obere Wand
         output *= wall
     end
-    output*="\n"
+    output*="\n" # Zeilenumbruch
     
-    for i=1:height
-        for k=1:2
+    for i=1:height # geht jede Zeile durch
+        for k=1:2 # macht immer zwei Dinge
             output*=wall
-            if k==1
-                for j=1:width
+            if k==1 # das erste geht die ungeraden Felder durch
+                for j=1:width # prüft ob Feld ein Path, Wand oder frei ist
                     if [j,i] in s
                         output*=path
                     else
@@ -56,8 +58,8 @@ function visualize(matrix::Matrix{Node}, Path::Union{Vector{Node},Nothing})
                 end
                 output*="\n"
 
-            else
-                for j=1:width
+            else # das zweite geht die geraden Felder durch
+                for j=1:width # prüft für jedes Feld ob path, wand oder frei
                     if n[i,j][3]===nothing
                         output*=wall
                     elseif (n[i,j][3] in s) & ([j,i] in s)
